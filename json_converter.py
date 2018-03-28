58c1b87f2e2888931aa138c286fe21283cb7fecd
isuramanchanayake <isura.14@cse.mrt.ac.lk>
2018-03-28 21:08:07 +0530

    Added some comments

--- json_converter.py
+++ json_converter.py
@@ -7,23 +7,65 @@ class JsonConverter:
     @abstractmethod
     @classmethod
     def _parse_post(cls, post_str):
+        """
+        Parse a social media post text to a dictionary object
+
+        Args:
+            post_str: social media post text string
+        Returns:
+            Parsed dictionary object
+        """
         pass
 
     @classmethod
     def process(cls, post_file_path, output_file_path):
+        """
+        Process an input file containing social media posts line by line and converts them to
+        a json array and writes to the specified output file
+
+        Args:
+            post_file_path: input file path
+            param output_file_path: output json file path
+        Raises:
+            IOError: file reading or writing error
+        """
         parsed_posts = []
-        with open(post_file_path, 'r') as tweet_file:
-            for line in tweet_file:
-                parsed_posts.append(cls._parse_post(line))
-        with open(output_file_path, 'w') as output_file:
-            output_file.write(json.dumps(parsed_posts.__dict__))
+        try:
+            with open(post_file_path, 'r') as tweet_file:
+                for line in tweet_file:
+                    parsed_posts.append(cls._parse_post(line))
+        except IOError:
+            raise IOError('IOError while reading')
+        try:
+            with open(output_file_path, 'w') as output_file:
+                output_file.write(json.dumps(parsed_posts))
+        except IOError:
+            raise IOError('IOError while writing')
+
 
 class TweetJsonConverter(JsonConverter):
     @classmethod
     def _parse_post(cls, post_str):
+        """
+        Parses a Tweet post string to a dictionary object
+        
+        Args:
+            post_str: tweet post text string
+        Returns:
+            Parsed dictionary object
+        """
         pass
 
 class TripAdvisorPostJsonConverter(JsonConverter):
     @classmethod
     def _parse_post(cls, post_str):
+        """
+        Parses a Trip Advisor post string to a dictionary object
+        
+        Args:
+            post_str: Trip Advisor post text string
+        Returns:
+            Parsed dictionary object
+        """
         pass
+
