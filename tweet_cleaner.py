558aae4c75b3623bcb4ad3596fa55268ca2cf548
Prabod Manuranga Rathnayaka <prabod@rathnayaka.me>
2018-03-29 10:10:32 +0530

    Update tweet_cleaner.py

--- tweet_cleaner.py
+++ tweet_cleaner.py
@@ -7,10 +7,10 @@ class TweetCleaner(SentenceParser):
 
     def parse(self, sentence):
         text = sentence
-	    text = re.sub(r'http\S+', '', text)
-	    text = re.sub(r'\&\S+;', '', text)
-	    text = re.sub(r'\\u[0-9]+', '', text)
-	    text = re.sub(r'@\S+', '', text)
-	    sentence = text.replace('#', '')
-	    return sentence
+		text = re.sub(r'http\S+', '', text)
+		text = re.sub(r'\&\S+;', '', text)
+		text = re.sub(r'\\u[0-9]+', '', text)
+		text = re.sub(r'@\S+', '', text)
+		sentence = text.replace('#', '')
+		return sentence
 
